d29bfd5baba2b613e1fed6d040542150f50f8fa4
platypusrex <frankcooke79@gmail.com>
2018-03-20 22:07:29 -0400

    Rework - removes Posts route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,7 +3,6 @@ import { NativeRouter, Switch, Route } from 'react-router-native';
 import { CheckToken } from './CheckToken';
 import { Register } from './Register';
 import { Login } from './Login';
-import { Posts } from './Posts';
 import { Tabs } from './Tabs';
 
 export const Routes: React.SFC<{}> = () => (
@@ -12,7 +11,6 @@ export const Routes: React.SFC<{}> = () => (
       <Route exact={true} path="/" component={CheckToken}/>
       <Route exact={true} path="/login" component={Login}/>
       <Route exact={true} path="/register" component={Register}/>
-      <Route exact={true} path="/posts" component={Posts}/>
       <Route exact={true} path="/tabs" component={Tabs}/>
     </Switch>
   </NativeRouter>
