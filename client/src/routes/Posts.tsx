a266bc1313558a9d8aee56fadc570d81e4244cd6
platypusrex <frankcooke79@gmail.com>
2018-03-20 22:06:35 -0400

    Rework - adds Navbar to each Tab route

--- client/src/routes/Posts.tsx
+++ client/src/routes/Posts.tsx
@@ -4,20 +4,27 @@ import { Text, View } from 'react-native';
 import { containerStyles as styles } from '../styles/shared/ContainerStyles';
 import { RouteComponentProps, withRouter } from 'react-router';
 import { Button } from '../shared/components/Button';
+import { Navbar } from '../shared/components/Navbar';
 
 type Props = RouteComponentProps<{}>;
 
 const PostsComponent: React.SFC<Props> = (props) => {
   return (
-    <View style={[styles.centered, styles.paddingLg]}>
-      <Text>Welcome to the '/posts' route!</Text>
-      <Button
-        type="primary"
-        full={true}
-        buttonText="sign off"
-        onPress={() => props.history.push('/login')}
-        style={{marginTop: 10}}
+    <View>
+      <Navbar
+        body={{title: 'Posts'}}
+        rightButton={{icon: {name: 'md-camera', size: 28}, handler: () => console.log('button clicked')}}
       />
+      <View style={[styles.centered, styles.paddingLg]}>
+        <Text>Welcome to the '/posts' route!</Text>
+        <Button
+          type="primary"
+          full={true}
+          buttonText="sign off"
+          onPress={() => props.history.push('/login')}
+          style={{marginTop: 10}}
+        />
+      </View>
     </View>
   );
 };
--- client/src/routes/Profile.tsx
+++ client/src/routes/Profile.tsx
@@ -4,20 +4,27 @@ import { Text, View } from 'react-native';
 import { containerStyles as styles } from '../styles/shared/ContainerStyles';
 import { RouteComponentProps, withRouter } from 'react-router';
 import { Button } from '../shared/components/Button';
+import { Navbar } from '../shared/components/Navbar';
 
 type Props = RouteComponentProps<{}>;
 
 const ProfileComponent: React.SFC<Props> = (props) => {
   return (
-    <View style={[styles.centered, styles.paddingLg]}>
-      <Text>Welcome to the '/profile' route!</Text>
-      <Button
-        type="primary"
-        full={true}
-        buttonText="sign off"
-        onPress={() => props.history.push('/login')}
-        style={{marginTop: 10}}
+    <View>
+      <Navbar
+        body={{title: 'Profile'}}
+        rightButton={{icon: {name: 'md-camera', size: 28}, handler: () => console.log('button clicked')}}
       />
+      <View style={[styles.centered, styles.paddingLg]}>
+        <Text>Welcome to the '/profile' route!</Text>
+        <Button
+          type="primary"
+          full={true}
+          buttonText="sign off"
+          onPress={() => props.history.push('/login')}
+          style={{marginTop: 10}}
+        />
+      </View>
     </View>
   );
 };
