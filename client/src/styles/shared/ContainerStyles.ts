d7d5099bbeceda8914c1fb21df94eab5cc46a908
platypusrex <frankcooke79@gmail.com>
2018-03-22 10:29:41 -0400

    Rework - adds 'full' container style

--- client/src/styles/shared/ContainerStyles.ts
+++ client/src/styles/shared/ContainerStyles.ts
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export const containerStyles = StyleSheet.create({
+  full: {
+    flex: 1
+  },
   centered: {
     flex: 1,
     alignItems: 'center',
