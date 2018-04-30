998ac35a1cb2dfe34b9c5bd1575e9431f17bffb6
platypusrex <frankcooke79@gmail.com>
2018-04-29 23:56:13 -0400

    Rework - adds refetchQueries prop to the create post mutations options

--- client/src/api/post/withCreatePost.ts
+++ client/src/api/post/withCreatePost.ts
@@ -1,6 +1,7 @@
 import gql from 'graphql-tag';
 import { graphql, QueryProps } from 'react-apollo';
 import { User } from '../../../../server/src/generated/prisma';
+import { allPostsQuery } from './withAllPosts';
 
 const createPostMutation = gql`
   mutation ($caption: String!, $picture: Upload!) {
@@ -43,10 +44,12 @@ export interface WithCreatePostProps {
 export const withCreatePost = graphql(createPostMutation, {
   props: ({ownProps, mutate}) => ({
     createPost: (variables: CreatePostInput) => {
-      console.log(variables);
       const options = {
         mutation: createPostMutation,
-        variables
+        variables,
+        refetchQueries: [
+          {query: allPostsQuery}
+        ],
       };
 
       if (!mutate) {
